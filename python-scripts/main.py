9bab3204bbf39a54d70c48a72871269af10ef441
ALeemans <anne.leemans@hu.nl>
2022-11-16 13:32:53 +0100

    accuracy test toegeveogd

--- python-scripts/main.py
+++ python-scripts/main.py
@@ -15,18 +15,18 @@ if __name__ == "__main__":
 
     # Boolean model halen
     from scripts.sentiment import sentimetBoolean
-    df_sent_bool = sentimetBoolean(df,0.95)
+    df_sent_bool = sentimetBoolean(df,0.95, 'onderwerp_1')
 
     # Likert scale model binnen halen
     from scripts.sentiment import sentimetLikert
-    df_sent_likert = sentimetLikert(df)
+    df_sent_likert = sentimetLikert(df, 'onderwerp_1')
 
     # data prepareren voor visualisatie
-    from scripts.visualisze import sentimentVisDfBool,sentimentVisDfLikert
+    from scripts.visualize import sentimentVisDfBool,sentimentVisDfLikert
     df_sent_bool_vis = sentimentVisDfBool(df_sent_bool, 'ref_specificatie')
     df_sent_likert_vis = sentimentVisDfLikert(df_sent_likert, 'ref_specificatie')
 
     # toon barplot
-    from scripts.visualisze import sentimentBarPlotBool,sentimentBarPlotLikert
+    from scripts.visualize import sentimentBarPlotBool,sentimentBarPlotLikert
     sentimentBarPlotBool(df_sent_bool_vis.head(15),'Sentiment van eerste vraag uit verzoek emails naar referentie Boolean   \n')
     sentimentBarPlotLikert(df_sent_likert_vis.head(15),'Sentiment van eerste vraag uit verzoek emails naar referentie Likert   \n')
